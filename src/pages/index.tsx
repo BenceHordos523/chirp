da4d7950431bbc02fb7c5039c11f48b0abd7e9d4
BenceHordos523 <Bence.Hordos523@gmail.com>
2023-04-09 12:08:42 +0200

    working posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,13 +9,22 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
 import { LoadingPage } from "~/components/loading";
+import { useState } from "react";
 
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
 
-  console.log(user);
+  const ctx = api.useContext();
+
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.posts.getAll.invalidate();
+    },
+  });
+  const [input, setInput] = useState("");
 
   if (!user) return null;
 
@@ -32,7 +41,11 @@ const CreatePostWizard = () => {
         type="text"
         placeholder="Type some emojis"
         className="grow bg-transparent outline-none"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
       />
+      <button onClick={() => mutate({ content: input })}>Post</button>
     </div>
   );
 };
@@ -74,7 +87,7 @@ const Feed = () => {
 
   return (
     <div className="flex flex-col">
-      {[...data, ...data]?.map((fullPost) => (
+      {data.map((fullPost) => (
         <PostView {...fullPost} key={fullPost.post.id} />
       ))}
     </div>
