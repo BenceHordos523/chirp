851428af98c871fdbf8660ff29ea4c96ac19c758
BenceHordos523 <Bence.Hordos523@gmail.com>
2023-04-09 14:34:57 +0200

    Profile and post page, profile router

---
+++ src/server/helpers/filterUserForClient.ts
@@ -0,0 +1,8 @@
+import type { User } from "@clerk/nextjs/dist/api";
+export const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    name: user.username,
+    profilePicture: user.profileImageUrl,
+  };
+};
